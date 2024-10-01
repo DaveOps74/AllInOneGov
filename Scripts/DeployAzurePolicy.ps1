3d91c9d0f79904c5744fc765298647a1e314af20
Dave Hollings <dave.hollings@kocho.co.uk>
2024-10-01 10:26:58 +0100

    New Script

---
+++ Scripts/DeployAzurePolicy.ps1
@@ -0,0 +1,53 @@
+### Configure Azure Activity logs to stream to specified Log Analytics workspace (Tenant Root Group) ###
+
+# Global Parameters
+$Location = "UK South"
+
+$SentinelResourceGroupName = "rg-sentinel-uks-400"
+$LogAnalyticsWorkspaceName = "law-sentinel-400"
+
+$KeyVaultResourceGroupName = "rg-sentinelkv-uks-400"
+
+$AzureActivityPolicyAssignmentName = "SOC AAL"
+$AzureActivityPolicyAssignmentDisplayName = "SIEM - Configure Azure Activity logs to stream to specified Log Analytics workspace"
+
+$AzureKeyVaultPolicyAssignmentName = "SOC KV"
+$AzureKeyVaultPolicyAssignmentDisplayName = "SIEM - Configure diagnostic settings for Azure Key Vault to Log Analytics workspace"
+
+$AzureLogicAppsPolicyAssignmentName = "SOC LAP"
+$AzureLogicAppsPolicyAssignmentDisplayName = "SIEM - Configure diagnostic settings for Azure Logic Apps to Log Analytics workspace"
+
+# Connect to your Azure account
+##Connect-AzAccount
+
+# Get Tenant ID
+$tenantId = (Get-AzContext).Tenant.Id
+
+# Get Subscription ID
+$subscriptionId = (Get-AzContext).Subscription.Id
+
+# Get Policy Definition(s)
+# Configure Azure Activity logs to stream to specified Log Analytics workspace #
+$AzureActivityPolicyDefinition = "/providers/Microsoft.Authorization/policyDefinitions/2465583e-4e78-4c15-b6be-a36cbc7c8b0f"
+# Deploy - Configure diagnostic settings for Azure Key Vault to Log Analytics workspace #
+$AzureKeyVaultPolicyDefinition = "/providers/Microsoft.Authorization/policyDefinitions/951af2fa-529b-416e-ab6e-066fd85ac459"
+# Deploy Diagnostic Settings for Logic Apps to Log Analytics workspace #
+$AzureLogicAppsPolicyDefinition = "/providers/Microsoft.Authorization/policyDefinitions/b889a06c-ec72-4b03-910a-cb169ee18721"
+
+# Set Scope for Policy Definitions
+$AzureActivityPolicyScope = "/providers/Microsoft.Management/managementGroups/$tenantId"
+$AzureKeyVaultPolicyScope = "/subscriptions/$subscriptionId/resourceGroups/$KeyVaultResourceGroupName"
+$AzureLogicAppsPolicyScope = "/subscriptions/$subscriptionId/resourceGroups/$SentinelResourceGroupName"
+
+# Get Log Analytics Workspace ID
+$logAnalyticsWorkspaceId = (Get-AzOperationalInsightsWorkspace -ResourceGroupName $SentinelResourceGroupName -Name $LogAnalyticsWorkspaceName).ResourceId
+
+# Create Policy Assignment(s)
+New-AzPolicyAssignment -Name $AzureActivityPolicyAssignmentName -DisplayName $AzureActivityPolicyAssignmentDisplayName -Scope $AzureActivityPolicyScope -policyDefinition $AzureActivityPolicyDefinition -PolicyParameterObject @{ logAnalytics = $logAnalyticsWorkspaceId } -IdentityType SystemAssigned -Location $Location
+New-AzPolicyAssignment -Name $AzureKeyVaultPolicyAssignmentName -DisplayName $AzureKeyVaultPolicyAssignmentDisplayName -Scope $AzureKeyVaultPolicyScope -policyDefinition $AzureKeyVaultPolicyDefinition -PolicyParameterObject @{ logAnalytics = $logAnalyticsWorkspaceId } -IdentityType SystemAssigned -Location $Location
+New-AzPolicyAssignment -Name $AzureLogicAppsPolicyAssignmentName  -DisplayName $AzureLogicAppsPolicyAssignmentDisplayName -Scope $AzureLogicAppsPolicyScope -policyDefinition $AzureLogicAppsPolicyDefinition -PolicyParameterObject @{ logAnalytics = $logAnalyticsWorkspaceId } -IdentityType SystemAssigned -Location $Location
+
+# Create a remediation task
+##Start-AzPolicyRemediation -Name "SOC AAL Rem" -PolicyAssignmentId $policyAssignment.PolicyAssignmentId
+
+
